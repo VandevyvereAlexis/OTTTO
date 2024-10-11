4474d0423c87146ad4c92f08e4c7405685d55e0c
VandevyvereAlexis <131187655+VandevyvereAlexis@users.noreply.github.com>
2024-10-11 15:09:19 +0200

    Création des models vides

---
+++ app/Models/Critair.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Critair extends Model
+{
+    use HasFactory;
+}
