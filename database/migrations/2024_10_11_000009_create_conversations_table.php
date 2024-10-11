4533662a1f9f98ce2c2e0bb4536862efcbf350e1
VandevyvereAlexis <131187655+VandevyvereAlexis@users.noreply.github.com>
2024-10-11 17:10:23 +0200

    Modification champs table Conversation

--- database/migrations/2024_10_11_000009_create_conversations_table.php
+++ database/migrations/2024_10_11_000009_create_conversations_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('initiateur_id')->nullable()->constrained('users')->onDelete('set null');
             $table->foreignId('destinataire_id')->nullable()->constrained('users')->onDelete('set null');
-            $table->foreignId('annonces_id')->nullable()->constrained('annonces')->onDelete('set null');
+            $table->foreignId('annonce_id')->nullable()->constrained('annonces')->onDelete('set null');
             $table->timestamps();
         });
     }
